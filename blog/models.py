c36e1d7a78884b9224aa1ccfafe6c1ce462ce690
lupk123 <zhaoxiaojuan1121@163.com>
2017-06-29 23:33:45 +0800

    initial blog app file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField("标题", max_length=30)
+    author = models.CharField("作者", max_length=30)
+    content = models.TextField()
+    date = models.DateTimeField("添加时间", auto_now_add=True)
+    modifyDate = models.DateTimeField("修改时间", auto_now=True)
+    label = models.CharField("标签", max_length=20)
+
+    class Meta:
+        db_table = "article"
+
+    def __str__(self):
+        return self.title
